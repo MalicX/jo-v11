aa39ac8fd3c5ae403919308359f311f48b3ee474
MalicX <115193236+MalicX@users.noreply.github.com>
2022-11-29 08:06:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,9 +22,9 @@ global.autorecording = false //status auto merekam ( auto record )
 global.autoketik = false //status auto mengetik (auto typing)
 global.available = false //status online (online)
 // Other
-global.owner = ['60177426853','60177426853','60177426853']
-global.premium = ['60177426853']
-global.packname = 'The Jo Bot'
+global.owner = ['6281387545144','6289515629775']
+global.premium = ['6281387545144']
+global.packname = 'Xilverz Mods'
 global.author = 'WhatsApp Bot'
 global.wlcm = []
 global.sessionName = 'session'
